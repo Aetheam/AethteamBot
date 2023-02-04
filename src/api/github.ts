1bffee1a08950a9f038d7ebb3f1664c908726e57
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-02-04 20:35:26 +0100

    ajout du vendeur à la répo github a tester

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,5 +1,7 @@
 import {getStringEnv} from "../utils/EnvVariable";
 import {messageLogger} from "../utils/BaseEmbed";
+import CommandsModel from "../models/CommandsModel";
+import UserModel from "../models/UserModel";
 const baseUrl: string = "https://api.github.com".toString();
 const organization: string = "Aetheam".toString()
 export const createRepository = async (name: string, description: string, webhook: string, channelId: string) =>{
@@ -39,13 +41,39 @@ export const createRepository = async (name: string, description: string, webhoo
                         "insecure_ssl":"0"}
                 })
             })
+            try{
+                const db = await CommandsModel.findOne({channelId: channelId});
+                if (db){
+                    const userProfil = await UserModel.findOne({discordId: db.sellerId});
+                    if (userProfil){
+                        const githubName = userProfil.githubName;
+                        await fetch(baseUrl + `repos/${resultJson.full_name}/collaborators/${githubName}`, {
+                            headers: {
+                                "Accept": "application/vnd.github+json",
+                                'authorization': 'Bearer ' + getStringEnv("GITHUB_TOKEN"),
+                                'content-type': 'application/json',
+                                'X-GitHub-Api-Version': '2022-11-28'
+                            },
+                            body: JSON.stringify({
+                                permission: "admin"
+                            })
+                        })
+                    }else {
+                        new Error("aucune donnée n'as été trouvé pour le channel. merci de vérifier le code ")
+                    }
+                }else {
+                    new Error("aucune donnée n'as été trouvé pour le channel. merci de vérifier le code ")
+                }
+            }catch (error){
+                await messageLogger(JSON.stringify(error,null, 2))
+            }
             //JSON.stringify(parse,null, 2)
         }catch (error){
-            await messageLogger(JSON.stringify(error))
+            await messageLogger(JSON.stringify(error,null, 2))
         }
     }catch (e){
         if (e instanceof TypeError){
-            await messageLogger(e.toString())
+            await messageLogger(JSON.stringify(e,null, 2))
         }
     }
 }
